6aea69f5f0077f7883b2a2a82be2ebfde74eb493
saeed srm <rahmani.0109@gmail.com>
2022-04-25 22:34:16 +0430

    restFramwork

--- basfum/urls.py
+++ basfum/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path ,include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter()
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    # path('api/', include('articles.urls', namespace='api')),
 ]
