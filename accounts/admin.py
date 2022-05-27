9b043c1cca4201da3c408c2dcea59579cb5f0a09
saeed srm <rahmani.0109@gmail.com>
2022-05-27 21:51:30 +0430

    microservices

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from accounts.models import Account
-
-
-@admin.register(Account)
-class AccountAdmin(admin.ModelAdmin):
-    pass
